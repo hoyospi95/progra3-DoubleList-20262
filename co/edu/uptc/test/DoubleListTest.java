05404749dbe6f29eed4f8b4b237937efd29b50ba
hoyospi95personal <hoyospi@gmail.com>
2026-08-20 15:24:06 -0500

    se adiciono TestSuitCase

---
+++ co/edu/uptc/test/DoubleListTest.java
@@ -0,0 +1,207 @@
+package co.edu.uptc.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import co.edu.uptc.structures.DoubleList;
+
+public class DoubleListTest {
+private List<String> list;
+
+	@BeforeEach
+	void setUpBeforeEach() throws Exception {
+		list = new DoubleList<String>();
+		list.add("juan");
+		list.add("lola");
+	}
+
+	@Test
+	void testSize() {
+		assertEquals(2, list.size());
+	}
+
+	@Test
+	void testIsEmpty() {
+		assertEquals(false, list.isEmpty());
+	}
+
+	@Test
+	void testContains() {
+		assertEquals(true, list.contains("lola"));
+	}
+
+	@Test
+	void testIterator() {
+		Iterator<String> iterator = list.iterator();
+		assertEquals(true, iterator.hasNext());
+		assertEquals("juan", iterator.next());
+		assertEquals("lola", iterator.next());
+	}
+
+	@Test
+	void testToArray() {
+		Object[] array = list.toArray();
+		assertEquals("juan", array[0]);
+	}
+
+	@Test
+	void testToArrayTArray() {
+		String[] array = list.toArray(new String[list.size()]);
+		assertEquals("lola", array[1]);
+		Object[] array2 = list.toArray(new Object[list.size()]);
+		assertEquals("lola", array2[1]);
+	}
+
+	@Test
+	void testAddT() {
+		list.add("laura");
+		assertEquals(true, list.contains("laura"));
+		assertEquals(3, list.size());
+	}
+
+	@Test
+	void testRemoveObject() {
+		list.remove("lola");
+		assertEquals(false, list.contains("lola"));
+	}
+
+	@Test
+	void testContainsAll() {
+		ArrayList<String> names = new ArrayList<String>();
+		names.add("juan");
+		names.add("lola");
+		boolean actual = list.containsAll(names);
+		assertEquals(true, actual);
+		names.add("lulu");
+		actual = list.containsAll(names);
+		assertEquals(false, actual);
+	}
+
+	@Test
+	void testAddAllCollectionOfQextendsT() {
+		ArrayList<String> names = new ArrayList<String>();
+		names.add("juanita");
+		names.add("lolita");
+		list.addAll(names);
+		assertEquals(true, list.contains("lolita"));
+	}
+
+	@Test
+	void testAddAllIntCollectionOfQextendsT() {
+		ArrayList<String> names = new ArrayList<String>();
+		names.add("juanita");
+		names.add("lolita");
+		list.addAll(1, names);
+		assertEquals(true, list.get(1).equals("juanita"));
+	}
+
+	@Test
+	void testRemoveAll() {
+		ArrayList<String> names = new ArrayList<String>();
+		names.add("juan");
+		list.removeAll(names);
+		assertEquals(true, list.get(0).equals("lola"));
+	}
+
+	@Test
+	void testRetainAll() {
+		ArrayList<String> namesToRetain = new ArrayList<>();
+		namesToRetain.add("lola");
+		list.retainAll(namesToRetain);
+		assertEquals(true, list.get(0).equals("lola"));
+	}
+
+	@Test
+	void testClear() {
+		list.clear();
+		assertEquals(0, list.size());
+	}
+
+	@Test
+	void testGet() {
+		assertEquals("lola", list.get(1));
+	}
+
+	@Test
+	void testSet() {
+		list.set(0, "pedro");
+		assertEquals("pedro", list.get(0));
+	}
+
+	@Test
+	void testAddIntT() {
+		list.add(2, "diana");
+		assertEquals("diana", list.get(2));
+	}
+
+	@Test
+	void testRemoveInt() {
+		list.remove(1);
+		assertFalse(list.contains("lola"));
+	}
+
+	@Test
+	void testIndexOf() {
+		assertEquals(0, list.indexOf("juan"));
+	}
+
+	@Test
+	void testLastIndexOf() {
+		list.add("juan");
+		assertEquals(2, list.lastIndexOf("juan"));
+	}
+
+	@Test
+	void testListIterator() {
+		ListIterator<String> iterator = list.listIterator();
+		assertTrue(iterator.hasNext());
+        assertEquals("juan", iterator.next());
+
+        assertTrue(iterator.hasNext());
+        assertEquals("lola", iterator.next());
+
+        assertFalse(iterator.hasNext());
+      
+        assertTrue(iterator.hasPrevious());
+        assertEquals("lola", iterator.previous());
+
+        assertTrue(iterator.hasPrevious());
+        assertEquals("juan", iterator.previous());
+
+        assertFalse(iterator.hasPrevious());
+        
+        iterator.next();
+        iterator.remove();
+        assertEquals(1, list.size());  
+        
+        iterator = list.listIterator();
+        iterator.next();
+        iterator.set("javier");
+        assertEquals("javier", list.listIterator().next());
+
+        
+        iterator.remove();
+        assertEquals(0, list.size()); 
+        
+        iterator.add("pablo");	
+		assertEquals("pablo", list.get(list.size()-1));
+	}
+
+
+
+	@Test
+	void testSubList() {
+		List<?> subList= list.subList(0, 1);
+		assertEquals(true, subList.get(0).equals("juan"));
+	}
+
+
+}
